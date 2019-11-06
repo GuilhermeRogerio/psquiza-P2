140889afcfa84b24e4ce3e3ed10f8fa154a0b81d
GuilhermeRogerio <guilherme.silva@ccc.ufcg.edu.br>
2019-11-06 11:49:41 -0300

    adicionei testes

--- src/controladores/Facade.java
+++ src/controladores/Facade.java
@@ -7,9 +7,9 @@ public class Facade {
 	private ControllerGeral controllerGeral;
 
 	public static void main(String[] args) {
-		args = new String[] { "controladores.Facade", "testes_aceitacao/use_case_2.txt",
-				"testes_aceitacao/use_case_3.txt", "testes_aceitacao/use_case_1.txt", "testes_aceitacao/use_case_4.txt", 
-				"testes_aceitacao/use_case_6.txt" };
+		args = new String[] { "controladores.Facade", "testes_aceitacao/use_case_1.txt",
+				"testes_aceitacao/use_case_2.txt", "testes_aceitacao/use_case_3.txt", "testes_aceitacao/use_case_4.txt", 
+				"testes_aceitacao/use_case_5.txt","testes_aceitacao/use_case_6.txt","testes_aceitacao/use_case_7.txt","testes_aceitacao/use_case_8.txt" };
 		EasyAccept.main(args);
 	}
 
